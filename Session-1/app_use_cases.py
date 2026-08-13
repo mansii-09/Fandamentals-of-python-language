4b8c69f73d67a623c33c2e3716ed3084bcc5cb85
Mansi goswami <goswamimansi2006@gmail.com>
2026-08-13 20:21:55 +0530

    Add files via upload

---
+++ Session-1/app_use_cases.py
@@ -0,0 +1,9 @@
+print("1. Instgram")
+print("   python can be for data analysis and recommendation system.")
+
+print("2. YouTube")
+print("   python can be used to analyze user activity and recommend videos.")
+
+print("3. Amazom")
+print("   python can be used for data analysis and product recommendation.")
+
